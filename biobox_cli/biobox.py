3c17f080a2e530618613aa98e22a52f5f9a8350e
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2015-11-09 22:25:44 +0100

    forbid mounting non empty output directories

--- biobox_cli/biobox.py
+++ biobox_cli/biobox.py
@@ -1,8 +1,10 @@
 from abc import ABCMeta, abstractmethod
 import biobox_cli.container   as ctn
 import biobox_cli.util.misc   as util
+import biobox_cli.util.error  as error
 import tempfile as tmp
 import inspect
+import os
 
 class Biobox:
     __metaclass__ = ABCMeta
@@ -21,6 +23,8 @@ class Biobox:
         task        = opts['--task']
         image       = opts['<image>']
         output      = opts['--output']
+        if os.listdir(output):
+            error.err_exit("non_empty_output_dir", { "dir": output})
         host_dst_dir = tmp.mkdtemp()
         volumes = self.prepare_volumes(opts, host_dst_dir)
         ctn.exit_if_no_image_available(image)
